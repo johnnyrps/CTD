6694911c3727e0a1cfdb4abf4b8c0d7a096363b4
johnnyrps <johnnyrps@gmail.com>
2021-11-10 21:03:24 -0300

    aula10-progimp

---
+++ programacaoimperativa/Aula10/arrays.js
@@ -0,0 +1,32 @@
+// let nome = ["Johnny", "Pecego", "Débora", "Mariana"];
+
+// let primeiroValor = nome.shift(); // .shift() elimina da array o primeiro valor e me retorna o primeiro valor eliminado do array, e ele não recebe parâmetro
+// console.log(nome);
+// console.log(primeiroValor);
+
+// //.push() - Adiciona um ou mais elementos ao final do array - recebe um ou mais elementos como parâmetros - retorna o novo comprimento do array
+
+// Arrays
+
+// Como podemos agrupar muitas informações em uma única variável? 
+
+// Para isso, temos um tipo de dado um pouco mais estruturado, denominado array, que armazena uma coleção de elementos, de modo que cada um deles pode ser identificado por um índice ou uma chave.
+
+// Esta estrutura também é conhecida como variável indexada, vetor (unidimensional) ou matriz (no caso de ser multidimensional).
+
+// -------
+// Métodos de Arrays
+
+
+// Agora que entendemos sobre arrays, vamos conhecer algumas funções que podemos utilizar com eles.
+
+// Com elas (funções), podemos interagir com os arrays, modificando seus valores e obtendo informações relacionadas a eles.
+
+// As funções que veremos serão: Push, Pop, Shift, Unshift, Join, IndexOf, LastIndexOf, Includes.
+//-------
+
+
+
+let produtoCompras = ["Beleza", "Limpeza", "Frutas", "Verduras", "Carnes"];
+
+let juntaElemetos()
