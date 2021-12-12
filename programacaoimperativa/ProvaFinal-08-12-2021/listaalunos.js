b4fe9265b7145b99d63c97478ee8fca718f11b01
johnnyrps <johnnyrps@gmail.com>
2021-12-12 12:02:05 -0300

    checkp3-front

---
+++ programacaoimperativa/ProvaFinal-08-12-2021/listaalunos.js
@@ -0,0 +1,11 @@
+let cadastro2 = require('./moduloaluno');
+
+    let listaAlunos = [];
+
+    listaAlunos.push(new cadastro2.cadastro('Johnny',0, [9, 10, 10]));
+    listaAlunos.push(new cadastro2.cadastro('Lucas',3, [10, 9.75, 10]));
+    listaAlunos.push(new cadastro2.cadastro('Jadson',1, [10, 10, 10]));
+    listaAlunos.push(new cadastro2.cadastro('Katy',3, [8, 7, 10]));
+    listaAlunos.push(new cadastro2.cadastro('Pedro',1, [8, 8, 7]));
+
+    module.exports = listaAlunos
