1e7b1218865c85d1add889ca605e1c7803ef150f
johnnyrps <johnnyrps@gmail.com>
2022-02-22 21:58:16 -0300

    aula2

---
+++ Front-end-II/front2-clone/02/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,21 @@
+//1 passo
+var nomeUsuario = prompt('Digite seu nome completo');
+
+//2 passo
+
+if ((nomeUsuario == null)||(nomeUsuario == "")) {
+    alert("O nome é obrigatório");
+
+}
+
+var telefoneAluno = prompt('Digite seu telefone com DDD')
+
+if (telefoneAluno){
+    
+}else {
+    alert("O telefone é obrigatório");
+}
+
+
+console.log(nomeUsuario);
+console.log(telefoneAluno);
