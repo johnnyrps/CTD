29657734941c4c16d596ee7d5605267ff6b1dee1
johnnyrps <johnnyrps@gmail.com>
2022-03-07 23:08:49 -0300

    novo

--- Front-end-II/front2-clone/04/mesa-de-trabalho/__entregaDaAtividade__.md
+++ Front-end-II/front2-clone/04/mesa-de-trabalho/__entregaDaAtividade__.md
@@ -1,4 +1,4 @@
-# Aula 02 - Entrega da atividade
+# Aula 04 - Entrega da atividade
 
 Adicione uma item na lista com o nome da dupla e link do repositório. Caso tenha dúvida, procure o professor, turo(a) e/ou auxiliar técnico.
 
---
+++ Front-end-II/front2-clone/04/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,15 @@
+let listas = document.querySelectorAll(".lista__item");
+listas[0].classList.add('removido')
+listas[0].innerText="Carro - Removido"
+
+listas[1].classList.add('selecionado')
+listas[1].innerText="Bike - Selecionado"
+
+listas[2].classList.add('selecionado')
+listas[2].innerText="Patinete - Selecionado"
+
+listas[3].classList.add('removido')
+listas[3].innerText="Celular - Removido"
+
+listas[4].classList.add('removido')
+listas[4].innerText="Carregador - Removido"
