b5f0ffaff2c8282e1c7ecd013f39096636c44d3c
johnnyrps <johnnyrps@gmail.com>
2022-03-21 20:47:35 -0300

    mesa10-front2

--- Front-end-II/front2-clone/10/mesa-de-trabalho/assets/js/main.js
+++ Front-end-II/front2-clone/10/mesa-de-trabalho/assets/js/main.js
@@ -1,10 +1,20 @@
 
 // 1. Selecione o formulário
-
-// 2. Crie da rotina que será disparada a partir do evento de envio
-
-// 2.2. Remova todos os espaços vazios
+var formulario = document.querySelector("#formularioDeCadastro");
+var campo = document.querySelector('.c-form__campo');
+var li = document.querySelector('.c-lista__item');
+formulario.addEventListener("submit", function (evento) {
+evento.preventDefault();
+    
+    var SemTratamento = evento.target[0].value;
+    var SemEspacos = SemTratamento.replace(/\d+/g, '');
+    alert("Texto sem Tratamento:" + SemTratamento);
+    var SemEspacos = SemTratamento.replace(/\s+/g, '');
 
 // 2.1. Remova todos os números permitindo apenas texto
+    var SemEspacos = SemTratamento.replace(/\d+/g, "");
 
 // 3. Capture o evento de envio do formulário e disparar a rotina
+li.innerText = SemEspacos;
+});
+
