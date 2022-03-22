ffcef9a8f052c686179d76a15699835981c97b27
johnnyrps <johnnyrps@gmail.com>
2022-03-21 23:30:03 -0300

    mesa10-correto

--- Front-end-II/front2-clone/10/mesa-de-trabalho/assets/js/main.js
+++ Front-end-II/front2-clone/10/mesa-de-trabalho/assets/js/main.js
@@ -4,17 +4,22 @@ var formulario = document.querySelector("#formularioDeCadastro");
 var campo = document.querySelector('.c-form__campo');
 var li = document.querySelector('.c-lista__item');
 formulario.addEventListener("submit", function (evento) {
-evento.preventDefault();
-    
-    var SemTratamento = evento.target[0].value;
-    var SemEspacos = SemTratamento.replace(/\d+/g, '');
-    alert("Texto sem Tratamento:" + SemTratamento);
-    var SemEspacos = SemTratamento.replace(/\s+/g, '');
+evento.preventDefault()
 
+// 2. Crie da rotina que será disparada a partir do evento de envio
+    var SemTratamento = evento.target[0].value;
+    
 // 2.1. Remova todos os números permitindo apenas texto
-    var SemEspacos = SemTratamento.replace(/\d+/g, "");
+    var SemTratamento = campo.value.replace(/\d+/g,'');
+
+// 2.2. Remova todos os espaços vazios
+    var SemEspacos = SemTratamento.replace(/\s+/g,'');
 
 // 3. Capture o evento de envio do formulário e disparar a rotina
-li.innerText = SemEspacos;
+    li.innerText = SemEspacos;
+
+// 4. Crie um crie um alerta
+    alert("Texto sem Tratamento: " + SemTratamento);
+
 });
 
