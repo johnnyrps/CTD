c7577bafef80aab8da5d3e82c8a50b23640ee494
johnnyrps <johnnyrps@gmail.com>
2022-04-03 12:25:57 -0300

    front2-mesa14

--- Front-end-II/front2-clone/14/mesa-de-trabalho/assets/js/scripts.js
+++ Front-end-II/front2-clone/14/mesa-de-trabalho/assets/js/scripts.js
@@ -1,47 +1,39 @@
+let form = document.forms['formulario'];
+let campo = document.querySelector('.c-form__campo');
+let btn = document.querySelector('.c-form__botao');
+let texto = document.querySelector('.texto');
 
-/*
+if (form) {
+    btn.disabled = false;
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        usuario = campo.value;
+        location.href = `http://127.0.0.1:5501/Front-end-II/front2-clone/14/mesa-de-trabalho/resultado/?nome=${usuario}`
 
-    Implemente seu algorítmo aqui...
+    })
+}
 
-    Resumo da atividade:
+let parametro = location.search;
 
-    1. Capture o nome do usuário a partir do campo `<input name="usuario">`:
+let consulta = new URLSearchParams(parametro);
 
+if (consulta.has('nome')) {
+    let nome = consulta.get('nome');
+    document.documentElement.lang = "en-US"
+    idioma = document.documentElement.lang = "en-US"
+    texto.innerText += ` ${nome}`
+    localStorage.setItem('nome', nome)
+    localStorage.setItem('idioma', idioma)
 
-    - 1.1. Armazene esse valor em uma variável;
+} else {
+    getStorage()
+}
 
-    - 1.2. Redirecione o usuário e envie o valor para rota `resultado/` com parametro `nome`.
-
-    2. Crie um parametro `lang` e defina o valor `br`. 
-
-    3. Capture o valor do parametro `usuario`:
-
-        - 1.1. Se houver o parametro `usuario` apresente o valor no elemento `<h1 class="texto">`
-
-        - 1.2. Caso contrário apresente apenas a saudação. 
-        
-            Nota: A saudação já esta presente no código HTML.
-
-    4. Armaze as informações no `localStorage` e persista as informações.
-
-    5. BONUS: Armazene as informações de linguagem, presente no parametro `lang`:
-
-        - 4.1. Altere o idioma da saudação
-
-            Exemplo: Olá, Hi, Holá, etc.
-
-            Nota: O intuito é realizar uma prova de conceito, portanto, não se preocupe com a quantidade de idiomas suportados.
-
-*/
-
-//var nomeUsuario = document.querySelector('input[name="usuario"]').value;
-console.log(nomeUsuario);
-
-//1.2. Redirecione o usuário e envie o valor para rota `resultado/` com parametro `nome`.
-var nomeUsuario = 'Johnny';
-var url = location.host;
-var rota = '/resultado?nome=' + nomeUsuario;
-var parametros = location.search;
-var consulta = new URLSearchParams(parametros);
-var elementHTML = document.querySelector('.texto');
-location.href = url+rota;
+function getStorage() {
+    if (texto) {
+        if (localStorage.length > 0) {
+            document.documentElement.lang = localStorage.getItem('idioma')
+            texto.innerText += ` ${localStorage.getItem('nome')}`
+        }
+    }
+}
